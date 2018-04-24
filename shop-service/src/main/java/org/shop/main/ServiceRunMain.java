3a4fab93ffd3417077e66f51bcab8c3853a258d9
zpp222 <38618638+zpp222@users.noreply.github.com>
2018-04-24 22:29:33 +0800

    修改启动命令...

--- shop-service/src/main/java/org/shop/main/ServiceRunMain.java
+++ shop-service/src/main/java/org/shop/main/ServiceRunMain.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ServiceRunMain {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
 		context.getStartupDate();
