551c6b381e312d11acd2683508dfacde04b283cf
zpp222 <38618638+zpp222@users.noreply.github.com>
2018-05-10 23:03:56 +0800

    update

--- shop-service/src/main/java/org/shop/hz/extractor/UserValueExtractor.java
+++ shop-service/src/main/java/org/shop/hz/extractor/UserValueExtractor.java
@@ -4,6 +4,7 @@ import org.shop.serviceI.dto.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.gson.Gson;
 import com.hazelcast.query.extractor.ValueCollector;
 import com.hazelcast.query.extractor.ValueExtractor;
 
@@ -13,13 +14,15 @@ public class UserValueExtractor extends ValueExtractor<User, String> {
 
 	@SuppressWarnings({"unchecked", "rawtypes"})
 	@Override
-	public void extract(User user, String id, ValueCollector collector) {
-		logger.info("Extractor begin with user'id {}, collector {} !", id, collector);
-		if (null == user || null == user.getId()) {
-		} else {
-			collector.addObject(user.getId());
-		}
-		logger.info("Extractor end with user'id {}, collector {} !", user.getId(), collector);
+	public void extract(User user, String userId, ValueCollector collector) {
+		Gson gson = new Gson();
+		logger.info("Extractor begin with user {} ,id {}, collector {} !", gson.toJson(user, User.class), user.getId(),
+				gson.toJson(collector, ValueCollector.class));
+
+		collector.addObject(user.getId());
+
+		logger.info("Extractor end with user {} ,id {}, collector {} !", gson.toJson(user, User.class), user.getId(),
+				gson.toJson(collector, ValueCollector.class));
 	}
 
 }
