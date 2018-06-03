8cbfe91945829b13fe7ba05f5ff27f453b6cf656
zpp222 <38618638+zpp222@users.noreply.github.com>
2018-06-03 14:16:29 +0800

    add mint-ui

--- shop-console/src/main/webapp/static/js/login/login.js
+++ shop-console/src/main/webapp/static/js/login/login.js
@@ -1,23 +1,3 @@
-var vm = new Vue({
-  el: '#login',
-  data: {
-	  message:"",
-	  user: {}
-  },
-  methods: {
-	  onSubmit: function (event) {
-		  // alert("login: " + this.user.name);
-		  this.$http.get("login/"+this.user.name,{params: this.user},{emulateJSON: true}).then(function(response){
-			  console.log(response);
-			  this.message = response.bodyText;
-			  // alert("succ:" + response.bodyText);
-		  }, function(response){
-			  this.message = response.status;
-		  });
-    },
-  }
-})
-
 // 基于全局Vue对象使用http
 // Vue.http.get('/someUrl', [options]).then(successCallback, errorCallback);
 // Vue.http.post('/someUrl', [body], [options]).then(successCallback, errorCallback);
@@ -25,3 +5,28 @@ var vm = new Vue({
 // 在一个Vue实例内使用$http
 // this.$http.get('/someUrl', [options]).then(successCallback, errorCallback);
 // this.$http.post('/someUrl', [body], [options]).then(successCallback, errorCallback);
+
+var vm = new Vue({
+	el : '#login',
+	data : {
+		message : "",
+		user : {}
+	},
+	methods : {
+		onSubmit : function(event) {
+			// alert("login: " + this.user.name);
+			this.$http.get("login/" + this.user.name, {
+				params : this.user
+			}, {
+				emulateJSON : true
+			}).then(function(response) {
+				console.log(response);
+				this.message = response.bodyText;
+				this.$toast(this.message);
+				// alert("succ:" + response.bodyText);
+			}, function(response) {
+				this.message = response.status;
+			});
+		},
+	}
+})
