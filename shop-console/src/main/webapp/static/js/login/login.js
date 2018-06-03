4e923eb2b93655dffab584abf76fd26b15ec5076
zpp222 <38618638+zpp222@users.noreply.github.com>
2018-06-03 11:29:06 +0800

    vue update.

--- shop-console/src/main/webapp/static/js/login/login.js
+++ shop-console/src/main/webapp/static/js/login/login.js
@@ -1,11 +1,27 @@
 var vm = new Vue({
   el: '#login',
   data: {
-    user: {}
+	  message:"",
+	  user: {}
   },
   methods: {
 	  onSubmit: function (event) {
-		  alert("login: " + this.user.name);
-    }
+		  // alert("login: " + this.user.name);
+		  this.$http.get("login/"+this.user.name,{params: this.user},{emulateJSON: true}).then(function(response){
+			  console.log(response);
+			  this.message = response.bodyText;
+			  // alert("succ:" + response.bodyText);
+		  }, function(response){
+			  this.message = response.status;
+		  });
+    },
   }
 })
+
+// 基于全局Vue对象使用http
+// Vue.http.get('/someUrl', [options]).then(successCallback, errorCallback);
+// Vue.http.post('/someUrl', [body], [options]).then(successCallback, errorCallback);
+
+// 在一个Vue实例内使用$http
+// this.$http.get('/someUrl', [options]).then(successCallback, errorCallback);
+// this.$http.post('/someUrl', [body], [options]).then(successCallback, errorCallback);
