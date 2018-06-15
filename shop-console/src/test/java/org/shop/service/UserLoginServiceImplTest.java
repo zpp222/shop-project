175bebc027f945a769bd63ea4fe048056e253dda
zpp222 <38618638+zpp222@users.noreply.github.com>
2018-06-15 12:20:55 +0800

    update

--- shop-console/src/test/java/org/shop/service/UserLoginServiceImplTest.java
+++ shop-console/src/test/java/org/shop/service/UserLoginServiceImplTest.java
@@ -1,15 +1,12 @@
 package org.shop.service;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Unit test for simple App.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:spring-context.xml" })
+// @RunWith(SpringJUnit4ClassRunner.class)
+// @ContextConfiguration(locations = { "classpath:spring-context.xml" })
 public class UserLoginServiceImplTest {
 
 	@Test
