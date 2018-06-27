4f8899d446dfae9a1145f4da10ba063a873c6b40
zpp222 <38618638+zpp222@users.noreply.github.com>
2018-06-27 09:34:36 +0800

    shiro + log

--- shop-console/src/main/java/org/shop/filter/MyCorsFilte.java
+++ shop-console/src/main/java/org/shop/filter/MyCorsFilte.java
@@ -1,5 +1,12 @@
 package org.shop.filter;
 
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,6 +16,13 @@ import org.springframework.web.filter.CorsFilter;
 public class MyCorsFilte extends CorsFilter {
 
 	static Logger logger = LoggerFactory.getLogger(MyCorsFilte.class);
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		super.doFilterInternal(request, response, filterChain);
+		logger.info("CorsFilte doFilterInternal {}", request.getRequestURI());
+	}
 
 	public MyCorsFilte() {
 		super(configurationSource());
